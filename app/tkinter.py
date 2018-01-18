cd5e9fd4cb5d2f4006c9524a5ba74f8f36b49bc3
arpitaj5 <ajena@dons.usfca.edu>
2018-01-18 13:06:24 -0800

    updated with reset

--- app/tkinter.py
+++ app/tkinter.py
@@ -1,56 +1,5 @@
 from Tkinter import Tk, Label, Button
-'''
-class MyFirstGUI:
-    def __init__(self, master):
-        self.master = master
-        master.title("A simple GUI")
-
-        self.label = Label(master, text="This is our first GUI!")
-        self.label.pack()
-
-        self.greet_button = Button(master, text="Greet", command=self.greet)
-        self.greet_button.pack()
-
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
-
-    def greet(self):
-        print("Greetings!")
-
-root = Tk()
-my_gui = MyFirstGUI(root)
-root.mainloop()
-'''
-
 from Tkinter import *
-'''
-def printSomething():
-
-    label = Label(root, text= "<a>Hey, I printed you!</a><a>Hey, I printed you!</a>")
-    label.pack() 
-
-root = Tk()
-button = Button(root, text="Show me the nearest stations", command=printSomething) 
-button.pack()
-
-root.mainloop()
-'''
-
-'''
-master = Tk()
-
-listbox = Listbox(master)
-listbox.pack()
-
-listbox.insert(END, "Nearest stations:")
-
-for item in ["one", "two", "three", "four"]:
-    listbox.insert(END, item)
-
-
-mainloop()
-'''
-
 
 
 fields = 'Location', 'hour', 'day'
@@ -87,27 +36,11 @@ def makeform(root, fields):
       ent.pack(side=RIGHT, expand=YES, fill=X)
       entries.append((field, ent))
    return entries
+  
 
-def stations_nearby(vals):
-    if str(vals[0]) == '101':
-        for item in ["one", "zero", "one"]:
-            listbox.insert(END, item)
-    elif str(vals[0]) == '102':
-        for item in ["one", "zero", "two"]:
-            listbox.insert(END, item) 
-    else:
-        for item in ["zero", "zero", "zero"]:
-            listbox.insert(END, item) 
-        
-        
-'''            
-def stations_nearby():
-    for item in items:
-        listbox.insert(END, item)
-'''    
-
-def print_station(item):
-     istbox.insert(END, item)        
+def reset_station():
+     listbox.delete(0, END)
+     listbox.insert(END, 'Plese provide me input!')        
             
 if __name__ == '__main__':
     root = Tk()
@@ -117,11 +50,15 @@ if __name__ == '__main__':
     root.bind('<Return>', (lambda event, e=ents: fetch(e))) 
     global vals
     vals = fetch(ents)
-    print vals 
+    print vals
+    
     b1 = Button(root, text='Show', command=(lambda e=ents: fetch(e)))
     b1.pack(side=LEFT, padx=5, pady=5)
-    #b2 = Button(root, text='Quit', command=root.quit)
-    #b2.pack(side=LEFT, padx=5, pady=5)
-    #button = Button(root, text="Show me the nearest stations", command=(lambda v=vals: stations_nearby(v))) 
-    #button.pack(side=LEFT, padx=5, pady=5)
+    lb = Listbox(root)
+    
+    b = Button(root, text="Reset",
+           command=reset_station)
+    b.pack(side=LEFT, padx=5, pady=5)
+
     root.mainloop()
+    
